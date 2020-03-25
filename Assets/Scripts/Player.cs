c95e951ddbdea09723422e92a0aee36ca4c638fe
loChris <44450518+loChris@users.noreply.github.com>
2020-03-25 13:06:01 -0500

    added all the files and already created a character and added some movement animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private PlayerAnimation _playerAnimation;
+    [SerializeField] private float _speed = 1.5f; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerAnimation = GetComponent<PlayerAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        
+        _rigidbody2D.velocity = new Vector2(moveHorizontal, moveVertical) * _speed;
+        
+        _playerAnimation.Move(moveHorizontal);
+    }
+}
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(float move)
+    {
+        _animator.SetFloat("Move", math.abs(move));
+    }
+    
+}
